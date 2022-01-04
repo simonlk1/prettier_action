43c8461e4568f226077d89f9dcb99b88272d27bf
Simon Kristiansen <simon@kristianzen.dk>
2022-01-04 15:56:36 +0100

    modify valid plugin regex

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,8 +59,8 @@ if [ -n "$INPUT_PRETTIER_PLUGINS" ]; then
     for plugin in $INPUT_PRETTIER_PLUGINS; do
         echo "Checking plugin: $plugin"
         # check regex against @prettier/xyz
-        if ! echo "$plugin" | grep -Eq '(@prettier\/)+(plugin-[a-z\-]+)'; then
-            echo "$plugin does not seem to be a valid @prettier/plugin-x plugin. Exiting."
+        if ! echo "$plugin" | grep -Eq '[@]?prettier[\/ -]plugin-[-a-z]+'; then
+            echo "$plugin does not seem to be a valid prettier plugin. Exiting."
             exit 1
         fi
     done
